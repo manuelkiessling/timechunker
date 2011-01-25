0e886691577115140beeec9ab0d82bd5cda439dc
Manuel Kiessling <kiessling@myhammer.de>
2011-01-25 16:25:00 +0100

    first test running

---
+++ lib/chunk_size.rb
@@ -0,0 +1,10 @@
+class ChunkSize
+
+    attr_reader :size, :type
+
+    def initialize(size, type)
+      @size = size
+      @type = type
+    end
+
+end
---
+++ lib/time_range.rb
@@ -0,0 +1,10 @@
+class TimeRange
+
+  attr_reader :start_time, :end_time
+
+  def initialize(start_time, end_time)
+    @start_time = start_time
+    @end_time = end_time
+  end
+
+end
