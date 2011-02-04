93aae7a68ed4c8ec6aef1551eb3ab5dafef46571
Manuel Kiessling <manuel@kiessling.net>
2011-02-04 17:35:20 +0100

    version 0.1.0 working as expected

--- lib/timechunker/chunker.rb
+++ lib/timechunker/chunker.rb
@@ -3,14 +3,15 @@ module Timechunker
 
     def get_chunks(timerange, chunksize)
       if chunksize.type == 'minutes'
-        number_of_chunks = ((timerange.end_time - timerange.start_time) / 60 / chunksize.size).round.to_i
+        number_of_chunks = ((timerange.end_time - timerange.start_time) / 60 / chunksize.size).ceil.to_i
         number_of_chunks = number_of_chunks + 1
 
         first_chunk = find_chunk(timerange.start_time, chunksize)
 
         chunks = []
         number_of_chunks.times do |i|
-          chunks << first_chunk + (i * 60 * chunksize.size)
+          chunk = first_chunk + (i * 60 * chunksize.size)
+          chunks << chunk unless chunk > timerange.end_time
         end
 
         return chunks
--- test/unit/timechunker/chunker_test.rb
+++ test/unit/timechunker/chunker_test.rb
@@ -9,7 +9,7 @@ class ChunkerTest < Test::Unit::TestCase
   def teardown
   end
 
-  def test_chunk_calculation_simple
+  def test_chunk_calculation_default
     expected = [Time.local(2011, 1, 24, 13, 55, 0),
                 Time.local(2011, 1, 24, 14, 0, 0),
                 Time.local(2011, 1, 24, 14, 5, 0),
@@ -22,7 +22,23 @@ class ChunkerTest < Test::Unit::TestCase
     actual = chunker.get_chunks(timerange, five_minutes)
 
     assert_equal(expected, actual)
+  end
 
+  def test_chunk_calculation_other
+    expected = [Time.local(2011, 1, 24, 14, 0, 0),
+                Time.local(2011, 1, 24, 14, 5, 0),
+                Time.local(2011, 1, 24, 14, 10, 0),
+                Time.local(2011, 1, 24, 14, 15, 0),
+                Time.local(2011, 1, 24, 14, 20, 0),
+                Time.local(2011, 1, 24, 14, 25, 0)]
+
+    timerange = Timechunker::Timerange.new(Time.local(2011, 1, 24, 14, 4, 0), Time.local(2011, 1, 24, 14, 26, 3))
+    five_minutes = Timechunker::Chunksize.new(5, 'minutes')
+
+    chunker = Timechunker::Chunker.new
+    actual = chunker.get_chunks(timerange, five_minutes)
+
+    assert_equal(expected, actual)
   end
 
   def test_chunk_calculation_singlechunk
@@ -35,7 +51,62 @@ class ChunkerTest < Test::Unit::TestCase
     actual = chunker.get_chunks(timerange, five_minutes)
 
     assert_equal(expected, actual)
+  end
 
+  def test_chunk_calculation_singlechunk_boundary
+    expected = [Time.local(2011, 1, 24, 13, 55, 0)]
+
+    timerange = Timechunker::Timerange.new(Time.local(2011, 1, 24, 13, 55, 0), Time.local(2011, 1, 24, 13, 59, 59))
+    five_minutes = Timechunker::Chunksize.new(5, 'minutes')
+
+    chunker = Timechunker::Chunker.new
+    actual = chunker.get_chunks(timerange, five_minutes)
+
+    assert_equal(expected, actual)
+  end
+
+  def test_chunk_calculation_twochunks_boundary
+    expected = [Time.local(2011, 1, 24, 13, 55, 0),
+                Time.local(2011, 1, 24, 14, 0, 0)]
+
+    timerange = Timechunker::Timerange.new(Time.local(2011, 1, 24, 13, 55, 0), Time.local(2011, 1, 24, 14, 0, 0))
+    five_minutes = Timechunker::Chunksize.new(5, 'minutes')
+
+    chunker = Timechunker::Chunker.new
+    actual = chunker.get_chunks(timerange, five_minutes)
+
+    assert_equal(expected, actual)
+  end
+
+  def test_chunk_calculation_fullhour
+    expected = [Time.local(2011, 1, 24, 13, 0, 0),
+                Time.local(2011, 1, 24, 13, 20, 0),
+                Time.local(2011, 1, 24, 13, 40, 0),
+                Time.local(2011, 1, 24, 14, 0, 0)]
+
+    timerange = Timechunker::Timerange.new(Time.local(2011, 1, 24, 13, 0, 0), Time.local(2011, 1, 24, 14, 0, 0))
+    twenty_minutes = Timechunker::Chunksize.new(20, 'minutes')
+
+    chunker = Timechunker::Chunker.new
+    actual = chunker.get_chunks(timerange, twenty_minutes)
+
+    assert_equal(expected, actual)
+  end
+
+  def test_chunk_calculation_uneven
+    expected = [Time.local(2011, 1, 24, 13, 0, 0),
+                Time.local(2011, 1, 24, 13, 21, 0),
+                Time.local(2011, 1, 24, 13, 42, 0),
+                Time.local(2011, 1, 24, 14, 3, 0),
+                Time.local(2011, 1, 24, 14, 24, 0)]
+
+    timerange = Timechunker::Timerange.new(Time.local(2011, 1, 24, 13, 0, 0), Time.local(2011, 1, 24, 14, 30, 0))
+    twentyone_minutes = Timechunker::Chunksize.new(21, 'minutes')
+
+    chunker = Timechunker::Chunker.new
+    actual = chunker.get_chunks(timerange, twentyone_minutes)
+
+    assert_equal(expected, actual)
   end
 
 end
